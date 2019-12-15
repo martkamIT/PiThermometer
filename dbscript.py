566cb2f17103565f96eb3e56f982801cd0606b8e
martkamIT <martyna.kamieniorz@gmail.com>
2019-12-15 20:01:36 +0100

    Test

---
+++ dbscript.py
@@ -0,0 +1,26 @@
+import sqlite3 as lite
+import w1thermsensor
+import time
+
+con = lite.connect('temp.db')
+sensor = w1thermsensor.W1ThermSensor()
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("CREATE TABLE temp(data INT, temperature INT)")
+    i = 1
+    while i < 20:
+        sens_temp = sensor.get_temperature()
+        dat_time = time.time()
+        
+        str = "INSERT INTO temp VALUES({}, {})"
+        str = str.format(dat_time, sens_temp)
+        print(str)
+        
+        cur.execute(str)
+        i+=1
+
+    
+    
+    
